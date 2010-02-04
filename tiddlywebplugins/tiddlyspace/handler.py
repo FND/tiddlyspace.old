eebeade4779296f5c2205f826bd14b436b375f4e
FND <fnd@oda.(none)>
2010-02-04 16:49:46 +0000

    minor adjustments
    whitespace and quote char corrections

--- tiddlywebplugins/tiddlyspace/handler.py
+++ tiddlywebplugins/tiddlyspace/handler.py
@@ -21,7 +21,7 @@ def home(environ, start_response):
     username = subdomain[0]
 
     if username:
-        type = "public" # TODO: if member, used private
+        type = 'public' # TODO: if member, use private
         recipe = Recipe('%s_%s' % (username, type))
         route = '%s/tiddlers.wiki' % recipe_url(environ, recipe)
         _redirect(route)
@@ -46,10 +46,10 @@ def post_space_handler(environ, start_response):
         port = ':%s' % host['port']
     else:
         port = ''
-    
+
     recipe = Recipe('%s_public' % space_name)
     new_space_uri = '%s/tiddlers.wiki' % recipe_url(environ, recipe)
-        
+
     start_response('303 See Other', [
         ('Location', new_space_uri),
         ('Content-type', 'text/plain')])
