88aa361813a635f80e1b766a0b4a08fafc08821b
Michael Mahemoff <michael@mahemoff.com>
2010-02-04 14:59:04 +0000

    done three bags

--- tiddlywebplugins/tiddlyspace/instance.py
+++ tiddlywebplugins/tiddlyspace/instance.py
@@ -1,5 +1,55 @@
 from tiddlywebwiki.instance import (instance_config, store_contents, store_structure)
 
+def create_space(name, members, public_recipe_bags=[]):
+
+  public_policy = {
+    "read": [],
+    "write": members,
+    "create": members,
+    "delete": members,
+    "manage": members,
+    "accept": members,
+    "owner": members
+  }
+
+  private_policy = {}
+  private_policy.update(public_policy)
+  private_policy["read"] = members
+
+  public_recipe_lines = map(lambda bag: (bag, ''), public_recipe_bags)
+
+  store_structure['bags'][name+'_public'] = {
+    'desc': 'TiddlyPocketBook',
+    'policy': public_policy
+  }
+
+  store_structure['recipes'][name+'_public'] = {
+    'desc': 'TiddlyPocketBook',
+    'policy': public_policy,
+    'recipe': public_recipe_lines
+  }
+
+  store_structure['bags'][name+'_private'] = {
+    'desc': 'TiddlyPocketBook',
+    'policy': private_policy
+  }
+
+  private_recipe_lines = []
+  private_recipe_lines.extend(public_recipe_lines)
+  private_recipe_lines.extend(['system',name+"_private"])
+  store_structure['recipes']['book_private_recipe'] = {
+    'desc': 'TiddlyPocketBook',
+    'policy': private_policy,
+    'recipe': [
+      ('system', ''),
+      (name+'_public', ''),
+      (name+'_private', '')
+    ]
+  }
+
+#######
+## main
+
 _osmosoft = 'psd ben martin jermolene fnd simon cdent rakugo mahemoff'.split()
 for username in _osmosoft:
     store_structure['users'][username] = {
@@ -7,45 +57,6 @@ for username in _osmosoft:
         'roles': ['ADMIN']
     }
 
-members = ['psd']
-public_policy = {
-  "read": [],
-  "write": members,
-  "create": members,
-  "delete": members,
-  "manage": members,
-  "accept": members,
-  "owner": members
-}
-
-private_policy = {}
-private_policy.update(public_policy)
-private_policy["read"] = members
-
-store_structure['bags']['book_public'] = {
-  'desc': 'TiddlyPocketBook',
-  'policy': public_policy
-}
-
-store_structure['recipes']['book_public_recipe'] = {
-  'desc': 'TiddlyPocketBook',
-  'policy': public_policy,
-  'recipe': [
-    ('book_public', '')
-  ]
-}
-
-store_structure['bags']['book_private'] = {
-  'desc': 'TiddlyPocketBook',
-  'policy': private_policy
-}
-
-store_structure['recipes']['book_private_recipe'] = {
-  'desc': 'TiddlyPocketBook',
-  'policy': private_policy,
-  'recipe': [
-    ('system', ''),
-    ('book_public', ''),
-    ('book_private', '')
-  ]
-}
+create_space('book',      ['psd'],   ['book_public'])
+create_space('book_demo', ['psd'],   ['book_public', 'book_demo_public'])
+create_space('osmobook',  _osmosoft, ['book_public', 'osmobook_public'])
