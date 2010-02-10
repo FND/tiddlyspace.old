322e3a1ff112faf15f7d57cf8ad1d65f10d7de97
Martin Budden <martinbudden@Martin-Buddens-MacBook-Pro-75.local>
2010-02-10 16:38:03 +0000

    Further test code

--- test/testCloneSpace.js
+++ test/testCloneSpace.js
@@ -23,32 +23,24 @@ config.macros.clone = {
 
 config.macros.clone.handler = function(place,macroName,params,wikifier,paramString,tiddler)
 {
-	var title = params[0] ? params[0] : config.macros.clone.title;
-	//#var title = config.macros.testMacro.title;
-	//#var btn = createTiddlyButton(place,this.label,this.prompt,this.onClick,null,null,this.accessKey);
-	var btn = createTiddlyButton(place,this.label,this.prompt,this.onClick);
-	btn.setAttribute("title",title);
-	btn.setAttribute("params",params.join("|"));
+	createTiddlyButton(place,this.label,this.prompt,this.onClick);
 };
 
 config.macros.clone.onClick = function(e)
 {
-	var title = this.getAttribute("title");
-	var params = this.getAttribute("params").split("|");
-	config.macros.clone.test(title,params);
+	var params = {host:config.macros.clone.host,original:config.macros.clone.original,manage:config.macros.clone.manage,owner:config.macros.clone.owner};
+	config.macros.clone.test(params);
 	return false;
 };
 
-config.macros.clone.test = function(title,params)
+
+config.macros.clone.test = function(params)
 {
 	clearMessage();
 	displayMessage("Testing");
-	var adaptor = new config.adaptors['tiddlyspace']();
-	var context = {};
-	context.status = true;
-	context.host = config.macros.clone.host;
-	context.original = config.macros.clone.original;
-	var ret = adaptor.getRecipe(context,null,config.macros.clone.testcallback);
+	var adaptor = new config.adaptors['tiddlyweb']();
+	var context = {status:true,host:params.host,recipe:params.original};
+	var ret = adaptor.getRecipes(context,params,config.macros.clone.testcallback);
 	if(ret !== true)
 		displayMessage(ret);
 };
@@ -57,20 +49,19 @@ config.macros.clone.testcallback = function(context,userParams)
 {
 	displayMessage("Testing callback");
 console.log('original recipe',context.recipe);
-	context.recipe.policy.manage = config.macros.clone.manage;
-	context.recipe.policy.owner = config.macros.clone.owner;
-	var recipes = context.recipe.recipe;
+
+	var recipes = context.recipes;
 	for(var i=0;i<recipes.length;i++) {
 		bag = recipes[i];
 		console.log('bag',bag);
-		if(bag[0]==config.macros.clone.original) {
-			bag[0] = config.macros.clone.clone;
+		if(bag[0]==userParams.original) {
+			bag[0] = userParams.clone;
 			break;
 		}
 	}
-	//console.log('clone recipe',context.recipe);
-	console.bag = config.macros.clone.original;
-	var ret = context.adaptor.getTiddlers(context,null,config.macros.clone.testcallback2);
+	context.format = '.json?fat=1';
+	context.workspace = 'bags/' + userParams.original;
+	var ret = context.adaptor.getTiddlerList(context,userParams,config.macros.clone.testcallback2);
 	if(ret !== true)
 		displayMessage(ret);
 };
@@ -79,8 +70,28 @@ config.macros.clone.testcallback2 = function(context,userParams)
 {
 	displayMessage("Testing callback2");
 console.log('callback2 tiddlers',context.tiddlers);
+	store.suspendNotifications();
 	for(var i=0;i<context.tiddlers.length;i++) {
-		console.log('tt',context.tiddlers[i].title);
+		var tiddler = context.tiddlers[i];
+		//if(!tiddler.created)
+		//	tiddler.created = tiddler.modified;
+		store.saveTiddler(tiddler.title, tiddler.title, tiddler.text, tiddler.modifier, tiddler.modified, tiddler.tags, tiddler.fields, true, tiddler.created);
+	}
+	store.resumeNotifications();
+	store.notifyAll();
+	refreshDisplay();
+	autoSaveChanges();
+
+	// copy the recipe to the new workspace
+	context.workspace = 'bags/' + userParams.clone;
+	context.recipe.policy = {manage:userParams.manage,owner:userParams.owner};
+//	context.adaptor.putRecipe(tiddler,context);
+
+	// now put the tiddlers to the new workspace
+	for(i=0;i<context.tiddlers.length;i++) {
+		tiddler = context.tiddlers[i];
+		tiddler.fields['server.workspace'] = context.workspace;
+//		context.adaptor.putTiddler(tiddler,context);
 	}
 };
 
