17e917bbf38b2ce6793823c29ea9bdbe4666f649
Michael Mahemoff <michael@mahemoff.com>
2010-02-10 15:55:09 +0000

    fleshed out build, now with _public and _private

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 instance="instance"
 if [ -d $instance ] ; then
   mv $instance /tmp/tiddlyspace-server-$$
@@ -13,8 +13,15 @@ for user in $osmosoft ; do
   twanager userpass $user x
 done
 
+twanager twimport book_public http://hoster.peermore.com/recipes/TiddlyPocketBook/tiddlers.wiki
+
+twanager twimport book_demo_public http://hoster.peermore.com/recipes/TiddlyPocketBook/tiddlers.wiki
+twanager twimport book_demo_public http://hoster.peermore.com/bags/TiddlyPocketBook%20-%20TiddlyWiki%20Cheatsheet/tiddlers.wiki
+
+twanager twimport osmobook_public http://hoster.peermore.com/bags/TiddlyPocketBook%20-%20TiddlyWiki%20Cheatsheet/tiddlers.wiki
+
 echo "*********************************************"
 echo "  Instance Complete"
 echo "*********************************************"
-echo "now you can run 'cd instance ; twanager server' and point your browser at http://0.0.0.0:8080."
+echo "now you can run 'cd instance ; twanager server' and point your browser at http://0.0.0.0:8080/."
 echo "To log in, visit http://0.0.0.0:8080 and type in 'username' (e.g. psd, cdent, ...) and password is 'x'"
--- tiddlywebplugins/tiddlyspace/instance.py
+++ tiddlywebplugins/tiddlyspace/instance.py
@@ -1,13 +1,17 @@
-#### TODO ensure ?bag_create_policy and ?recipe_create_policy are false (for production)
-
 from tiddlywebwiki.instance import (instance_config, store_contents, store_structure)
 
+#### TODO ensure ?bag_create_policy and ?recipe_create_policy are false (for production)
+#### (in tiddlywebconfig)
 
-instance_config['system_plugins'] = ['tiddlywebplugins.tiddlyspace']
-instance_config['twanager_plugins'] = ['tiddlywebplugins.tiddlyspace']
-
+#=======================================================================
+# create space
+#=======================================================================
 
 def create_space(name, members, public_recipe_bags=[]):
+
+  ###################################################################
+  # Set up stock policies
+  ###################################################################
 
   public_policy = {
     "read": [],
@@ -23,7 +27,16 @@ def create_space(name, members, public_recipe_bags=[]):
   private_policy.update(public_policy)
   private_policy["read"] = members
 
+  ###################################################################
+  # Set up recipes
+  ###################################################################
+
+  public_recipe_bags.append("_public")
   public_recipe_lines = map(lambda bag: (bag, ''), public_recipe_bags)
+
+  ###################################################################
+  # Set up public space
+  ###################################################################
 
   store_structure['bags'][name+'_public'] = {
     'desc': 'todo',
@@ -36,6 +49,10 @@ def create_space(name, members, public_recipe_bags=[]):
     'recipe': public_recipe_lines
   }
 
+  ###################################################################
+  # Set up private space
+  ###################################################################
+
   store_structure['bags'][name+'_private'] = {
     'desc': 'todo',
     'policy': private_policy
@@ -43,8 +60,8 @@ def create_space(name, members, public_recipe_bags=[]):
 
   private_recipe_lines = []
   private_recipe_lines.extend(public_recipe_lines)
-  private_recipe_lines.extend(['system',name+"_private"])
-  store_structure['recipes'][name+'_private_recipe'] = {
+  private_recipe_lines.extend(['system','_private',name+"_private"])
+  store_structure['recipes'][name+'_private'] = {
     'desc': 'todo',
     'policy': private_policy,
     'recipe': [
@@ -54,18 +71,54 @@ def create_space(name, members, public_recipe_bags=[]):
     ]
   }
 
-#######
-## main
+#=======================================================================
+# main
+#=======================================================================
 
-store_contents['system'].append('http://github.com/FND/tiddlyspace/raw/master/src/backstageClone.recipe') # XXX: should not pollute TiddlyWebWiki bag
+###################################################################
+## define policy const
+###################################################################
+
+open_policy = { "read": [], "write": [], "create": [], "delete": [], "manage": [], "accept": [], "owner": [] }
+
+###################################################################
+## set up global public bag
+###################################################################
+
+store_structure['bags']['_public'] = {
+  'desc': 'Public tiddlyspace bag - contains tiddlywiki (client-side) plugins for tiddlyspace public recipes, e.g. browsing and searching the whole server; and cloning',
+  'policy': open_policy
+}
+
+store_contents['_public'] = ['http://github.com/FND/tiddlyspace/raw/master/src/backstageClone.recipe']
+
+###################################################################
+## set up global private bag
+###################################################################
+
+store_structure['bags']['_private'] = {
+  'desc': 'Private tiddlyspace bag - same as _public bag, but for inclusion in *private* recipes, e.g. managing membership',
+  'policy': open_policy
+}
+
+###################################################################
+## set up sample users and their spaces
+###################################################################
 
 _osmosoft = 'psd ben martin jermolene fnd simon cdent rakugo mahemoff'.split()
 for username in _osmosoft:
-    store_structure['users'][username] = {
-        'note': 'Osmosoftonian',
-        'roles': ['ADMIN']
-    }
+  store_structure['users'][username] = {
+      'note': 'Osmosoftonian',
+      'roles': ['ADMIN']
+  }
+  create_space(username, [username], [username+'_public'])
+
+###################################################################
+## set up sample content spaces
+###################################################################
 
 create_space('book',      ['psd'],   ['book_public'])
 create_space('book_demo', ['psd'],   ['book_public', 'book_demo_public'])
 create_space('osmobook',  _osmosoft, ['book_public', 'osmobook_public'])
+
+# store_contents['book_public'].append('http://hoster.peermore.com/recipes/TiddlyPocketBook.tiddlywiki')
