eecc098907e3aba665021df53cb6ab0a82043c2a
Simon <user@notebook.(none)>
2010-02-10 15:30:24 +0000

    sdfsdfsdf

--- src/cloneWizard.js
+++ src/cloneWizard.js
@@ -2,6 +2,10 @@ config.macros.cloneSpace = {
 	'wizardTitle':'Clone Space',
 	'step1Title':'enter new space name',
 	'step1Html':'<input name="cloneName">',
+	'cloneButtonText':'Clone Space', 
+	'cloneButtonCaption':'click to clone this space',
+	'createButtonText':'Create New Space',
+	'createButtonCaption':'click to create a new space'
 		
 };
 config.macros.cloneSpace.handler = function(place,macroName,params,wikifier,paramString,tiddler) {
@@ -10,11 +14,28 @@ config.macros.cloneSpace.handler = function(place,macroName,params,wikifier,para
 	w.createWizard(place,me.wizardTitle);
 	w.addStep(me.step1Title,me.step1Html);
 	w.setButtons(
-	[{caption: 'Create New Space', tooltip: 'click to create a new space', onClick: function() {me.doClone(w);} },
-	{caption: 'Clone Space', tooltip: 'click to clone this space', onClick: function() {me.doClone(w);} }
+	[{caption: me.createButtonText, tooltip: me.createButtonCaption, onClick: function() {me.doCreate(w);} },
+	{caption: me.cloneButtonText, tooltip: me.cloneButtonCaption, onClick: function() {me.doClone(w);} }
 ]
 )
 }
+
+config.macros.cloneSpace.doCreate = function(w) {
+	var spaceName = w.getValue('cloneName').value;
+
+	// call bens code 
+	jQuery.ajax({url:'/spaces', data:{'name':spaceName}, success:function(response) {
+		if(response.status==='303')
+			alert('space has been created');
+		else 
+			alert('there was a problem creating the space');
+	}, error:function(response){
+			alert('there was a problem creating your space');
+	}});
+
+	w.addStep('created', 'created '+spaceName);
+
+};
 
 config.macros.cloneSpace.doClone = function(w) {
 	console.log(w.getValue('cloneName').value);
