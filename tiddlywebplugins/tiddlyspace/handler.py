154c4e86619fb2541df1adfa376ea4a85c761276
bengillies <bengillies@gmail.com>
2010-02-10 15:02:45 +0000

    first stab at implementing add user to space and remove user from space

--- tiddlywebplugins/tiddlyspace/handler.py
+++ tiddlywebplugins/tiddlyspace/handler.py
@@ -8,13 +8,16 @@ import urllib
 from cgi import FieldStorage
 
 from tiddlyweb.model.recipe import Recipe
-from tiddlyweb.web.http import HTTP302, HTTP404, HTTP409
+from tiddlyweb.web.http import HTTP302, HTTP404, HTTP409, HTTP400, HTTP403
 from tiddlyweb.web.util import recipe_url, server_host_url
 from tiddlyweb.web.handler import root
 from tiddlyweb.web.handler.recipe import get_tiddlers
 from tiddlywebplugins.utils import require_any_user
+from tiddlyweb.model.policy import ForbiddenError, UserRequiredError
+from tiddlyweb.store import NoUserError, NoBagError, NoRecipeError
 
 from space import Space, BagExistsError, RecipeExistsError
+from users import add_user, remove_user
 
 
 def home(environ, start_response):
@@ -74,3 +77,53 @@ def post_space_handler(environ, start_response):
         ('Content-type', 'text/plain')])
 
     return [new_space_uri]
+
+@require_any_user()
+def post_adduser_to_space_handler(environ, start_response):
+    """
+    add the given user to the space named in the url
+    """
+    space_name = environ['wsgiorg.routing_args'][1]['space_name']
+    if 'user' not in environ['tiddlyweb.query']:
+        raise HTTP400('No user supplied')
+    user_to_add = environ['tiddlyweb.query']['user'][0]
+    
+    try:
+        add_user(environ, space_name, user_to_add)
+    except NoUserError:
+        raise HTTP400('User does not exist: %s' % user_to_add)
+    except NoBagError, NoRecipeError:
+        raise HTTP404('Space not found: %s' % space_name)
+    except ForbiddenError, UserRequiredError:
+        raise HTTP403('You do not have permission to add %s to %s' % (user_to_add, space_name))
+        
+    start_response('204 No Content', [
+        ('Content-type', 'text/plain')
+    ])
+    
+    return []
+
+@require_any_user()
+def post_removeuser_from_space_handler(environ, start_response):
+    """
+    remove the given user from the space named in the url
+    """
+    space_name = environ['wsgiorg.routing_args'][1]['space_name']
+    if 'user' not in environ['tiddlyweb.query']:
+        raise HTTP400('No user supplied')
+    user_to_remove = environ['tiddlyweb.query']['user'][0]
+    
+    try:
+        remove_user(environ, space_name, user_to_remove)
+    except NoUserError:
+        raise HTTP400('User does not exist: %s' % user_to_add)
+    except NoBagError, NoRecipeError:
+        raise HTTP404('Space not found: %s' % space_name)
+    except ForbiddenError, UserRequiredError:
+        raise HTTP403('You do not have permission to remove %s from %s' % (user_to_remove, space_name))
+        
+    start_response    ('204 No Content', [
+            ('Content-type', 'text/plain')
+        ])
+    
+    return []
