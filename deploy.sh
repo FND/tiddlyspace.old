4ced43ff8ecac05cc60c0d8854a5bbeeebf39f69
FND <fnd@oda.(none)>
2010-03-04 14:48:08 +0000

    changed deploy script to use dist tarball

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
+host="tiddlyspace.labs.osmosoft.com"
 instance_dir="/data/vhost/tiddlyspace.labs.osmosoft.com/tiddlyweb"
-timestamp=`date +"%Y-%m-%d_%H%M%S"`
 
-echo "updating repository and rebuilding package" && \
 git pull && \
-python setup.py sdist && \
-pip install -U --no-deps dist/*tiddlyspace*tar.gz && \
-echo "creating instance" && \
-./dev.sh && \
-echo "backing up existing instance" && \
-mv $instance_dir "backup/instance_$timestamp" && \
-echo "moving over instance" && \
-mv instance $instance_dir && \
-cp -f backup/tiddlywebconfig.py $instance_dir/ && \
-curl -o $instance_dir/apache.py http://github.com/tiddlyweb/tiddlyweb/raw/master/apache.py && \
-chown -R fnd:www-data /data/vhost/tiddlyspace.labs.osmosoft.com && \
-apache2ctl restart
+make dist && \
+scp dist/*tiddlyspace*tar.gz "$host:~/tmp/" && \
+ssh $host "cd ~/tmp && sudo pip install -U *tiddlyspace*tar.gz && " \
+	"cd $instance_dir && sudo twanager update && sudo apache2ctl restart"
