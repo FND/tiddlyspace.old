586ce7fad7684e6250293077758fd73a085a91ff
FND <fnd@oda.(none)>
2010-03-04 16:00:06 +0000

    various adjustments to deployment script
    wildcards over SSH are dangerous

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Usage:
+#   $ ./deploy.sh [quick]
+
 host="tiddlyspace.labs.osmosoft.com"
 instance_dir="/data/vhost/tiddlyspace.labs.osmosoft.com/tiddlyweb"
 
@@ -9,11 +12,12 @@ fi
 
 git pull && \
 make dist && \
-scp dist/*tiddlyspace*tar.gz "$host:~/tmp/" && \
+scp dist/*tiddlyspace*tar.gz "$host:~/tmp/tiddlyspace.tar.gz" && \
 ssh $host "cd ~/tmp &&
 	echo 'installing' &&
-	sudo pip install $pip_options -U *tiddlyspace*tar.gz &&
+	sudo pip install $pip_options -U tiddlyspace.tar.gz &&
 	echo 'updating' &&
 	cd $instance_dir && sudo twanager update &&
+	sudo chown -R fnd:www-data $instance_dir &&
 	echo 'restarting' &&
 	sudo apache2ctl restart"
