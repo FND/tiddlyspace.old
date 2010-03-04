aecd7e0521772add9b56141307e3855d282ab812
Simon <user@notebook.(none)>
2010-03-04 14:20:16 +0000

    removing backups

--- src/zzTiddlySpace.js
+++ src/zzTiddlySpace.js
@@ -1,2 +1,10 @@
 store.addNotification("TiddlySpaceStyleSheet", refreshStyles);
 store.addNotification("jquery.autocomplete", refreshStyles);
+
+
+var oldRegisterCallback = config.adaptors.tiddlyweb.registerCallback;
+config.adaptors.tiddlyweb.registerCallback = function(username) {
+	oldRegisterCallback();
+	console.log('done register stuff');
+	spaceStore.put(new Space(username, [], [username]));
+}
